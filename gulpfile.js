bfc35d468410f572a9e67b157684d136e4e9b743
CREW <dev@crew.pt>
2019-06-23 05:33:46 +0100

    yeap

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@
 // START Editing Project Variables.
 // Project related.
 var project                 = 'dimakin'; // Project Name.
-var projectURL              = 'localhost/dimack'; // Local project URL of your already running WordPress site. Could be something like local.dev or localhost:8888.
+var projectURL              = 'localhost/dimakin'; // Local project URL of your already running WordPress site. Could be something like local.dev or localhost:8888.
 var productURL              = './'; // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 
 // Translation related.
