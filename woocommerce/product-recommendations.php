9981bef35d60de12d8f70cdffe18bb1d284d3066
CREW <dev@crew.pt>
2019-06-25 03:41:59 +0100

    upup

--- woocommerce/product-recommendations.php
+++ woocommerce/product-recommendations.php
@@ -32,15 +32,10 @@ if(!empty($product_recommendations)) { ?>
                    <?php endif; ?>
                  </header>
                  <section class="card-content">
-                   <?php
-                   if ( is_home() ) {
-                     $post_date = get_the_date( 'd-m-Y' );
-                     echo '<span class="posted-on">' , $post_date , '</span>';
-                   }
-                   ?>
                    <?php $product_title = get_the_title(); ?>
                    <h3 class="card-title"><?php echo mb_strimwidth( $product_title, 0, 40, '...' ); ?></h3>
-                   <?php the_excerpt(); ?>
+                   <?php $excerpt = the_excerpt(); ?>
+                   <?php echo mb_strimwidth( $excerpt, 0, 80, '...' ); ?>
                    <a href="<?php the_permalink(); ?>" class="card-link"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
                  </section>
                </article>
