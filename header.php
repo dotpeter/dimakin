a6245a70669fe153254f4512a4835e968c04533d
Pedro Protest <pedro.protest@gmail.com>
2020-03-12 09:35:17 +0000

    Added google tags

--- header.php
+++ header.php
@@ -33,10 +33,32 @@
 
   <?php wp_head(); ?>
 
-  <?php //$google_analytics = get_theme_mod( 'google_analytics' ); echo !empty($google_analytics) ? $google_analytics : ''; ?>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WHLKG3B');</script>
+  <!-- End Google Tag Manager -->
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160310617-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-160310617-1');
+  </script>
 
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-  <?php get_template_part('template-parts/main', 'header'); ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHLKG3B" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <?php
+  wp_body_open();
+  get_template_part('template-parts/main', 'header');
