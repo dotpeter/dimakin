a2f72fa1b7bbdc935a3348be302c18b2d1f50adb
Pedro Protest <pedro.protest@gmail.com>
2020-03-18 16:59:41 +0000

    added google gtags

--- header.php
+++ header.php
@@ -6,16 +6,21 @@
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
 <head>
-	<meta charset="utf-8">
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WL47PH7');</script>
+	<!-- End Google Tag Manager -->
 
+	<meta charset="utf-8">
 	<?php // force Internet Explorer to use the latest rendering engine available ?>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
 	<?php // mobile meta (hooray!) ?>
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
 	<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
 	<?php do_action( 'dimakin_touch_icons' );  ?>
 	<link rel="icon" href="<?php get_site_icon_url(); ?>">
@@ -26,14 +31,15 @@
 	<meta name="msapplication-TileColor" content="#3481b9">
 	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">
 	<meta name="theme-color" content="#3481b9">
-
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
 	<?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WL47PH7"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 
 	<?php
 	wp_body_open();
