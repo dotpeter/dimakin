9ad81238b440ed351e6a5e7ec9f4dffc8d3d646a
Pedro Protest <pedro.protest@gmail.com>
2020-09-03 15:39:45 +0100

    change products form
    Updated form on the product page, to a modal form.

--- inc/customizer/products-settings.php
+++ inc/customizer/products-settings.php
@@ -5,43 +5,43 @@
 
 
 // Settings
-$wp_customize->add_setting( 'product_btn_contact_url', array(
-  'default' => '',
-  'type' => 'theme_mod',
-  'capability' => 'edit_theme_options',
-  'transport' => 'refresh',
-  'sanitize_callback' => 'esc_html',
+$wp_customize->add_setting( 'product_btn_contact_form', array(
+	'default' => '',
+	'type' => 'theme_mod',
+	'capability' => 'edit_theme_options',
+	'transport' => 'refresh',
+	'sanitize_callback' => 'esc_html',
 ) );
 //Controls
 $wp_customize->add_control( new WP_Customize_Control(
-  $wp_customize,
-  'product_btn_contact_url',
-    array(
-      'type' => 'text',
-      'section'    => 'general_settings_section',
-      'priority'   => 3,
-      'label' => __( 'Url do botão na página de produtos', 'dimakin' ),
-      'description' => ''
-    )
+	$wp_customize,
+	'product_btn_contact_form',
+		array(
+			'type' => 'text',
+			'section'    => 'general_settings_section',
+			'priority'   => 3,
+			'label' => __( 'Shortcode do formulário de produto', 'dimakin' ),
+			'description' => ''
+		)
 ));
 
 // Settings
 $wp_customize->add_setting( 'product_videos_url', array(
-  'default' => '',
-  'type' => 'theme_mod',
-  'capability' => 'edit_theme_options',
-  'transport' => 'refresh',
-  'sanitize_callback' => 'esc_html',
+	'default' => '',
+	'type' => 'theme_mod',
+	'capability' => 'edit_theme_options',
+	'transport' => 'refresh',
+	'sanitize_callback' => 'esc_html',
 ) );
 //Controls
 $wp_customize->add_control( new WP_Customize_Control(
-  $wp_customize,
-  'product_videos_url',
-    array(
-      'type' => 'text',
-      'section'    => 'general_settings_section',
-      'priority'   => 3,
-      'label' => __( 'Url do botão para mais videos', 'dimakin' ),
-      'description' => ''
-    )
+	$wp_customize,
+	'product_videos_url',
+		array(
+			'type' => 'text',
+			'section'    => 'general_settings_section',
+			'priority'   => 3,
+			'label' => __( 'Url do botão para mais videos', 'dimakin' ),
+			'description' => ''
+		)
 ));
--- single-product.php
+++ single-product.php
@@ -11,55 +11,63 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 
+$product_contact_form = get_theme_mod( 'product_btn_contact_form' );
+
 do_action('dimakin_breadcrumbs');
 
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 ?>
 <main class="main-wrapper">
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="product-content-wrapper">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-md-6">
-            <?php get_template_part('template-parts/products/product', 'gallery'); ?>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="product-details">
-              <h1 class="product-title"><?php the_title(); ?></h1>
-              <?php the_content(); ?>
-              <div class="product-actions">
-                <?php
-                  $getQote = esc_url(get_theme_mod( 'product_btn_contact_url' ));
-                  $pdf = wp_get_attachment_url(get_post_meta( get_the_ID(), '_dimakin_products_catalog_id', true));
-                  if(!empty($getQote)) {
-                    echo '<a href="', $getQote , '" class="primary-btn" > <i class="fa fa-comments-o" aria-hidden="true"></i> ' , __('Fale Connosco', 'dimakin') , '</a>';
-                  }
-                  if(!empty($pdf)) {
-                    echo '<a href="', esc_url($pdf) , '" class="secondary-btn" target="_blank"> <i class="fa fa-file-pdf-o" aria-hidden="true"></i> ' , __('Catálogo', 'dimakin') , '</a>';
-                  }
-                ?>
-              </div><!-- product-actions -->
-              <?php
-                $productsTags = get_the_terms( get_the_ID(), 'post_tag' );
-                if ( $productsTags && ! is_wp_error( $productsTags) ) {
-                    echo '<p class="product-tags"><i class="fa fa-tags" aria-hidden="true"></i><strong>', __('Tags: ', 'dimakin') ,'</strong>';
-                    foreach ($productsTags as $tag) {
-                        $tagTitle = $tag->name; // tag name
-                        $tag_link = get_term_link( $tag );// tag archive link
-                        echo '<span><a href="', $tag_link ,'">', $tagTitle ,'</a></span>';
-                    }
-                    echo '</p>';
-                }
-              ?>
-            </div><!-- product-details -->
-          </div><!-- col -->
-        </div><!-- row -->
-      </div><!-- container -->
-    </div><!-- product-content-wrapper -->
-    <?php get_template_part('template-parts/products/product', 'videos'); ?>
-    <?php get_template_part('template-parts/products/product', 'recommendations'); ?>
-  </article><!-- article -->
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<div class="product-content-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-6">
+						<?php get_template_part( 'template-parts/products/product', 'gallery' ); ?>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="product-details">
+							<h1 class="product-title"><?php the_title(); ?></h1>
+							<?php the_content(); ?>
+							<div class="product-actions">
+								<a class="primary-btn" data-fancybox data-src="#product-buttons__modal-form" href="javascript:;" >
+									<i class="fa fa-comments-o" aria-hidden="true"></i>
+									<?php esc_html_e( 'Fale Connosco', 'dimakin' ); ?>
+								</a>
+								<div style="display: none;" id="product-buttons__modal-form">
+									<div id="modal-form">
+										<p class="modal-form__title"><?php esc_html_e( 'Obtenha mais informações sobre o produto.', 'dimakin' ); ?></p>
+										<?php echo do_shortcode( $product_contact_form ); ?>
+									</div><!-- #more-info-form-wrapper -->
+								</div><!-- #hidden-form -->
+								<?php
+								$pdf = wp_get_attachment_url( get_post_meta( get_the_ID(), '_dimakin_products_catalog_id', true ) );
+								if(!empty($pdf)) {
+									echo '<a href="', esc_url($pdf) , '" class="secondary-btn" target="_blank"> <i class="fa fa-file-pdf-o" aria-hidden="true"></i> ' , __( 'Catálogo', 'dimakin' ) , '</a>';
+								}
+								?>
+							</div><!-- product-actions -->
+							<?php
+								$productsTags = get_the_terms( get_the_ID(), 'post_tag' );
+								if ( $productsTags && ! is_wp_error( $productsTags) ) {
+										echo '<p class="product-tags"><i class="fa fa-tags" aria-hidden="true"></i><strong>', __('Tags: ', 'dimakin') ,'</strong>';
+										foreach ($productsTags as $tag) {
+												$tagTitle = $tag->name; // tag name
+												$tag_link = get_term_link( $tag );// tag archive link
+												echo '<span><a href="', $tag_link ,'">', $tagTitle ,'</a></span>';
+										}
+										echo '</p>';
+								}
+							?>
+						</div><!-- product-details -->
+					</div><!-- col -->
+				</div><!-- row -->
+			</div><!-- container -->
+		</div><!-- product-content-wrapper -->
+		<?php get_template_part('template-parts/products/product', 'videos'); ?>
+		<?php get_template_part('template-parts/products/product', 'recommendations'); ?>
+	</article><!-- article -->
 </main><!-- main-wrapper -->
 <?php
 endwhile;
